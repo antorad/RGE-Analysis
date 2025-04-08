83c28de4f661511d8980a813b56ce4213aa8e653
antorad <66285305+antorad@users.noreply.github.com>
2025-04-08 17:37:57 -0400

    Update run_simple_plots.sh
    commented the part to process each individual run separately

--- run_simple_plots.sh
+++ run_simple_plots.sh
@@ -3,24 +3,24 @@
 # Array of files to read wich contain run numbers for each target
 files=("C_runs.txt" "Al_runs.txt" "Cu_runs.txt" "Sn_runs.txt" "Pb_runs.txt")
 
-# Run simple_plots for each individual run
-for input_file in "${files[@]}"
-do
-    # Check if the file exists
-    if [[ ! -f $input_file ]]; then
-        echo "File not found: $input_file"
-        continue
-    fi
-
-    echo "Processing file: $input_file"
-
-    # Read the file line by line
-    while IFS= read -r line
-    do
-        # Process the line
-        root -l -q "simple_plots.cxx($line)"
-    done < "$input_file"
-done
+## Run simple_plots for each individual run
+#for input_file in "${files[@]}"
+#do
+#    # Check if the file exists
+#    if [[ ! -f $input_file ]]; then
+#        echo "File not found: $input_file"
+#        continue
+#    fi
+#
+#    echo "Processing file: $input_file"
+#
+#    # Read the file line by line
+#    while IFS= read -r line
+#    do
+#        # Process the line
+#        root -l -q "simple_plots.cxx($line)"
+#    done < "$input_file"
+#done
 
 # Run simple_plots for each target
 root -l -q 'simple_plots.cxx("C_runs.txt", "C")'
