c297a0656ce3463f9d552a564f6fb45a39ddc0d1
antorad <66285305+antorad@users.noreply.github.com>
2025-04-11 16:46:01 -0400

    Update plot_mr_all.cpp
    Removed bin by bin plot and moved to a new macro.

--- plot_mr_all.cpp
+++ plot_mr_all.cpp
@@ -1,16 +1,63 @@
 #include "include.h"
 
-void plot_mr_all(int Hadron_pid=211, TString mainVar="Zh"){
+TString mainVar="";
+TString hadron="";
+
+TCanvas plot_same_canvas(TH1D* h_mr[5]){
+    //canvas
+    TCanvas *canvas= new TCanvas("canvas","canvas",1000,600);
+    canvas->cd();
+    canvas->SetGrid();
+
+    //set range on y axis for each case
+    if (mainVar=="Zh") {h_mr[0]->GetYaxis()->SetRangeUser(0,1.6);}
+    if (mainVar=="Zh" && hadron=="proton") {h_mr[0]->GetYaxis()->SetRangeUser(0,6);}
+    if (mainVar=="Nu") {h_mr[0]->GetYaxis()->SetRangeUser(0.6,1.2);}
+    if (mainVar=="Nu" && hadron=="proton") {h_mr[0]->GetYaxis()->SetRangeUser(1,4);}
+
+    //Set axis names
+    h_mr[0]->GetYaxis()->SetTitle("#frac{N_{A}#pi^{+}}{N_{D2}#pi^{+}}#frac{N_{D2}e^{-}}{N_{A}e^{-}}");
+    h_mr[0]->GetXaxis()->SetTitle(mainVar);
+
+    //set colors
+    h_mr[0]->SetMarkerColor(1);
+    h_mr[0]->SetLineColor(1);
+    h_mr[1]->SetMarkerColor(2);
+    h_mr[1]->SetLineColor(2);
+    h_mr[2]->SetMarkerColor(4);
+    h_mr[2]->SetLineColor(4);
+    h_mr[3]->SetMarkerColor(209);
+    h_mr[3]->SetLineColor(209);
+    h_mr[4]->SetMarkerColor(205);
+    h_mr[4]->SetLineColor(205);
+
+    //draw plots without horizontal error bars
+    for (int i = 0; i < 5; ++i){
+        h_mr[i]->Draw("E1X0same");
+    }
+
+    //Draw legens in the top right corner
+    TLegend* legend = new TLegend(0.75, 0.75, 0.9, 0.9);
+    legend->AddEntry(h_mr[0], "Carbon", "p");
+    legend->AddEntry(h_mr[1], "Aluminum", "p");
+    legend->AddEntry(h_mr[2], "Copper", "p");
+    legend->AddEntry(h_mr[3], "Tin", "p");
+    legend->AddEntry(h_mr[4], "Lead", "p");
+    legend->Draw("same");
+
+    //Save final plot as pdf
+    canvas->SaveAs("output/mr_"+hadron+"_"+mainVar+".pdf");
+    return canvas;
+}
+
+void plot_mr_all(int Hadron_pid=211, TString Var="Zh"){
 	gStyle->SetOptStat(0);
-////////////////////////////////////////////////////////////////////////
-//////////          	   Total MR comparison	              //////////
-////////////////////////////////////////////////////////////////////////
+    mainVar=Var;
 
     //output directory
     TString output_location = "output/";
 
     //hadron selection
-    TString hadron;
     if (Hadron_pid==211){hadron="pion";}
     else if (Hadron_pid==-211){hadron="pion_minus";}
     else if (Hadron_pid==2212){hadron="proton";}
@@ -25,58 +72,11 @@ void plot_mr_all(int Hadron_pid=211, TString mainVar="Zh"){
     TFile* input_files[5];
     TH1D* h_mr_corr[5];
 
+    //Total MR comparison
     for (int i = 0; i < 5; ++i){
         input_files[i] = new TFile(output_location+"/"+targets[i]+"/mr_"+mainVar+"_"+hadron+".root","READ");
         h_mr_corr[i] = (TH1D*)input_files[i]->Get("MR_corr");
     }
-
-    //canvas
-    TCanvas *canvas= new TCanvas("canvas","canvas",1000,600);
-    canvas->cd();
-    canvas->SetGrid();
-
-    //set range on y axis for each case
-    if (mainVar=="Zh") {h_mr_corr[0]->GetYaxis()->SetRangeUser(0,1.6);}
-    if (mainVar=="Zh" && hadron=="proton") {h_mr_corr[0]->GetYaxis()->SetRangeUser(0,6);}
-    if (mainVar=="Nu") {h_mr_corr[0]->GetYaxis()->SetRangeUser(0.6,1.2);}
-    if (mainVar=="Nu" && hadron=="proton") {h_mr_corr[0]->GetYaxis()->SetRangeUser(1,4);}
-
-    //Set axis names
-    h_mr_corr[0]->GetYaxis()->SetTitle("#frac{N_{A}#pi^{+}}{N_{D2}#pi^{+}}#frac{N_{D2}e^{-}}{N_{A}e^{-}}");
-    h_mr_corr[0]->GetXaxis()->SetTitle(mainVar);
-
-    //set colors
-    h_mr_corr[0]->SetMarkerColor(1);
-    h_mr_corr[0]->SetLineColor(1);
-    h_mr_corr[1]->SetMarkerColor(2);
-    h_mr_corr[1]->SetLineColor(2);
-    h_mr_corr[2]->SetMarkerColor(4);
-    h_mr_corr[2]->SetLineColor(4);
-    h_mr_corr[3]->SetMarkerColor(209);
-    h_mr_corr[3]->SetLineColor(209);
-    h_mr_corr[4]->SetMarkerColor(205);
-    h_mr_corr[4]->SetLineColor(205);
-
-    //draw plots without horizontal error bars
-    for (int i = 0; i < 5; ++i){
-        h_mr_corr[i]->Draw("E1X0same");
-    }
-
-    //Draw legens in the top right corner
-    TLegend* legend = new TLegend(0.75, 0.75, 0.9, 0.9);
-    legend->AddEntry(h_mr_corr[0], "Carbon", "p");
-    legend->AddEntry(h_mr_corr[1], "Aluminum", "p");
-    legend->AddEntry(h_mr_corr[2], "Copper", "p");
-    legend->AddEntry(h_mr_corr[3], "Tin", "p");
-    legend->AddEntry(h_mr_corr[4], "Lead", "p");
-    legend->Draw("same");
-
-    //Save final plot as pdf
-    canvas->SaveAs("output/mr_"+hadron+"_"+mainVar+".pdf");
-
-////////////////////////////////////////////////////////////////////////
-//////////         	   Bin by bin MR comparison	              //////////
-////////////////////////////////////////////////////////////////////////
-
+    plot_same_canvas(h_mr_corr);
 }
-//Create a n array with the 5 target plots ans  then create functions with the array and output file as argument 
+//Create a n array with the 5 target plots and then create functions with the array and output file as argument 
